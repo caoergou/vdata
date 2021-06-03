7a75482da21689badd6413c6eb3e63dd19e8a62d
caoergou <1395762792@qq.com>
2021-06-03 21:49:23 +0800

    更新

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def stu(data, uid):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8084, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
--- test.py
+++
@@ -1,5 +0,0 @@
-import ujson as json
-with open(f'./feature/audio/hour_mean.json', 'r', encoding='utf-8') as f3:
-    t = f3.read()
-    print(len(t))
-    print(t)
